b2d86d7af1c8f275c5e90361443cf8baf2db4cc3
sefwx <151843085+sefwx@users.noreply.github.com>
2023-12-07 15:08:28 +0100

    Added projects and thumbnails from OpenProcessing

--- projects/01MainAssignments/project1/script.js
+++ projects/01MainAssignments/project1/script.js
@@ -1,29 +1,87 @@
-let len = 10;
-let bg;
+let switchWidth = 100;
+let switchHeight = 50;
+let switchColorOn = "#8EE4AF";
+let switchColorOff = "#F76D57";
+let switchState = false;
+let switchX, switchY;
+let switchSpeedX, switchSpeedY;
+let escapeRadius = 70;
+let escapeSpeed = 0.3; // aggiustare x velocità di scappata
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(10, 200, 58);
+  createCanvas(windowWidth, windowHeight);
+  resetSwitch();
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(20,250, 40);
-	
-	rect(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-	rect(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(89, 30, 33);
-	}
+function draw() {
+  background(255);
+
+  // movimento dello switch
+  switchX += switchSpeedX;
+  switchY += switchSpeedY;
+
+  // salto dagli spigoli della canvas
+  if (switchX < 0 || switchX > width) {
+    switchSpeedX *= -1;
+  }
+  if (switchY < 0 || switchY > height) {
+    switchSpeedY *= -1;
+  }
+
+  // controlla la distanza dal mouse
+  let distanceToMouse = dist(switchX, switchY, mouseX, mouseY);
+
+  // scappa se il mouse è vicino
+  if (distanceToMouse < escapeRadius) {
+    let escapeDirection = createVector(switchX - mouseX, switchY - mouseY);
+    escapeDirection.normalize();
+    escapeDirection.mult(escapeSpeed); // velocità
+    switchSpeedX += escapeDirection.x;
+    switchSpeedY += escapeDirection.y;
+  }
+
+  // disegna toggle switch
+  let switchColor = switchState ? switchColorOn : switchColorOff;
+  fill(switchColor);
+  rect(switchX - switchWidth / 2, switchY - switchHeight / 2, switchWidth, switchHeight, 10);
+
+  // disegna switch handle
+  fill(255);
+  let handleX = switchState ? switchX + switchWidth / 4 : switchX - switchWidth / 4;
+  ellipse(handleX, switchY, switchHeight * 1.2);
+
+  // txt istruzioni
+  fill(0);
+  textSize(16);
+  textAlign(CENTER, CENTER);
+  text("Click to toggle the floating switch!", width / 2, height - 50);
 }
 
+function mouseClicked() {
+  // controlla se l'utente ha schiacchiato il handle
+  let handleX = switchState ? switchX + switchWidth / 4 : switchX - switchWidth / 4;
+  let handleRadius = switchHeight * 0.6;
+  let distance = dist(mouseX, mouseY, handleX, switchY);
+
+  if (distance < handleRadius) {
+    // stato toggle switch e cambio colore
+    switchState = !switchState;
+    resetSwitch(); // reset posizione dello switch
+  }
+}
+
+function resetSwitch() {
+  // posizione iniziale dello switch
+  switchX = random(width);
+  switchY = random(height);
+
+  // velocità casuali
+  switchSpeedX = random(1, 3) * (random() > 0.5 ? 1 : -1);
+  switchSpeedY = random(1, 3) * (random() > 0.5 ? 1 : -1);
+}
+
+// chiama la funzione n sottofondo
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  resetSwitch(); 
+}
--- projects/01MainAssignments/project2/script.js
+++ projects/01MainAssignments/project2/script.js
@@ -1,29 +1,104 @@
-let len = 10;
-let bg;
+let columns = 12;
+let maxColumns = columns*columns;
+
+let letters = [];
+
+let pColumnsX = 0;
+let pColumnsY = 0;
+
+let maxChars = 30;
+
+var font;
+
+function preload() {
+	font = loadFont("assets/helvetica.otf");
+}
+
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(200, 0, 58);
+  createCanvas(1000, 1000);
+	textFont(font);
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(0,130, 140);
-	
-	ellipse(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-  	ellipse(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(189, 3, 33);
-	}
+function draw() {
+  background(0);
+  stroke(255);
+  strokeWeight(1);
+
+  //let columnsX = floor(map(mouseX, 0, width, 2, columns));
+  //let columnsY = floor(map(mouseY, 0, height, 2, columns));
+  
+  let columnsX = letters.length+2;
+  let columnsY = letters.length+2;
+ 
+  // grande / numero di linee
+  let ecartW = width / columnsX;
+  let ecartH = height / columnsY;
+
+  // linee verticali
+  if (mouseIsPressed) {
+    for (i = 1; i < columnsX; i++) {
+      line(i * ecartW, 0, i * ecartW, height);
+    }
+    // linee orizzontali 
+    for (i = 1; i < columnsY; i++) {
+      line(0, i * ecartH, width, i * ecartH);
+    }
+  }
+
+  let count = 0;
+  
+  for (i = 1; i <columnsY ; i++) {
+    for (j = 1; j < columnsX; j++) {
+			if( letters.length>0){
+      letter(count % letters.length, j * ecartW, i * ecartH);
+			
+      count++;
+			}
+    }
+  }
+  
+  pColumnsX = columnsX;
+  pColumnsY = columnsY;
 }
 
+function letter(index, posX, posY) {
+  push();
+  // trasformazioni
+  translate(posX, posY);
+  
+  let x = mouseX - posX;
+  let y = mouseY - posY;
+  
+  let angle = atan2(y, x);
+  
+  rotate(angle);
+  fill(255);
+  
+  if(index % 2 == 0) {
+    noFill();
+    stroke(255);
+    strokeWeight(1);
+  }
+  
+
+  textSize(map(letters.length, 1, maxChars, 128, 20));
+  textAlign(CENTER, CENTER);
+  text(letters[index], 0,0)
+  
+  pop(); 
+}
+
+function keyTyped() {
+ 
+  if (key.length <= 1 && letters.length < maxChars) {
+    letters.push(key);
+  }
+}
+
+function keyPressed(){
+  if(keyCode == BACKSPACE){
+    letters.pop();
+  }
+  
+}
